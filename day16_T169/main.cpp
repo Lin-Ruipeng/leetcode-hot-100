269afbebb322d48fb7f30ad9c445160b4272ab66
MuXinYue <likeszm@163.com>
2026-03-06 23:50:05 +0800

    Day16_T169_多数元素

---
+++ day16_T169/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+class Solution
+{
+public:
+    int majorityElement(vector<int> &nums)
+    {
+        sort(nums.begin(), nums.end());
+        return nums[nums.size() / 2];
+    }
+};
