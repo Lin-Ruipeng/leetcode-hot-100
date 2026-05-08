d7f0f3d0fa83ec19146c1a4711cfc84865698157
Fox Lin <likeszm@foxmail.com>
2026-05-08 14:14:56 +0800

    feat(carl/t349): the intersection of the two arrays

---
+++ Carl/Chapter05/T349.cpp
@@ -0,0 +1,21 @@
+// T349 两个数组的交集
+
+#include <unordered_set>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+ public:
+  vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+    unordered_set<int> result_set;  // 存放结果
+    unordered_set<int> nums_set(nums1.begin(), nums1.end());
+    for (auto num : nums2) {
+      // 均出现过就是交集
+      if (nums_set.find(num) != nums_set.end()) {
+        result_set.insert(num);
+      }
+    }
+    return vector<int>(result_set.begin(), result_set.end());
+  }
+};
