7c7430118dd0de7c8eaf33ec7f12cf25bd75e3b6
Fox Lin <likeszm@foxmail.com>
2026-05-11 15:27:48 +0800

    feat(carl/t150): 实现逆波兰表达式求值功能

---
+++ Carl/Chapter07/T150.cpp
@@ -0,0 +1,33 @@
+// T150. 逆波兰表达式求值
+
+#include <algorithm>
+#include <stack>
+#include <string>
+#include <vector>
+using namespace std;
+
+class Solution {
+ public:
+  int evalRPN(vector<string>& tokens) {
+    stack<int> st;
+    for (int i = 0; i < tokens.size(); ++i) {
+      if (tokens[i] == "+" || tokens[i] == "-" || tokens[i] == "*" ||
+          tokens[i] == "/") {
+        int num1 = st.top();
+        st.pop();
+        int num2 = st.top();
+        st.pop();
+        if (tokens[i] == "+") st.push(num2 + num1);
+        if (tokens[i] == "-") st.push(num2 - num1);
+        if (tokens[i] == "*") st.push(num2 * num1);
+        if (tokens[i] == "/") st.push(num2 / num1);
+      } else {
+        // stoi : "string to int"
+        st.push(stoi(tokens[i]));
+      }
+    }
+    int result = st.top();
+    st.pop();
+    return result;
+  }
+};
