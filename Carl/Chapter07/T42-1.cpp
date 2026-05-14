349b8053f132a3ca64951bc35af9bc17cb82c94f
Fox Lin <likeszm@foxmail.com>
2026-05-14 19:05:03 +0800

    feat(T42): 实现接雨水问题的双指针法

---
+++ Carl/Chapter07/T42-1.cpp
@@ -0,0 +1,24 @@
+// T42.接雨水 双指针法（超时）
+#include <vector>
+
+using namespace std;
+
+class Solution {
+ public:
+  int trap(vector<int>& height) {
+    int sum = 0;
+    for (int i = 1; i < height.size() - 1; ++i) {
+      int rHeight = height[i];
+      int lHeight = height[i];
+      for (int r = i + 1; r < height.size(); ++r) {
+        if (height[r] > rHeight) rHeight = height[r];
+      }
+      for (int l = i - 1; l >= 0; l--) {
+        if (height[l] > lHeight) lHeight = height[l];
+      }
+      int h = min(lHeight, rHeight) - height[i];
+      if (h > 0) sum += h;
+    }
+    return sum;
+  }
+};
