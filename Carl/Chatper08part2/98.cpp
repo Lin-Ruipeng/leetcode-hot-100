7e6a578859f4488fc93fd30ff7be02b1ac42a17b
Fox Lin <likeszm@foxmail.com>
2026-06-06 12:30:53 +0800

    feat(T98): 新增层序遍历法实现验证搜索二叉树的功能

--- Carl/Chatper08part2/98.cpp
+++ Carl/Chatper08part2/98.cpp
@@ -26,3 +26,29 @@ class Solution {
     return left && right;
   }
 };
+
+// 层序遍历法
+#include <stack>
+using namespace std;
+class Solution {
+ public:
+  bool isValidBST(TreeNode *root) {
+    stack<TreeNode *> st;
+    TreeNode *cur = root;
+    TreeNode *pre = nullptr;  // 前一个遍历的节点
+    while (cur || !st.empty()) {
+      if (cur) {
+        st.push(cur);
+        cur = cur->left;  // 左节点
+      } else {
+        cur = st.top();  // 中
+        st.pop();
+        if (pre && cur->val <= pre->val) return false;
+        pre = cur;  // 保存上一个
+
+        cur = cur->right;  // 访问右节点
+      }
+    }
+    return true;
+  }
+};
