df168bb43de47e88a4df382d8cebb6b3b9101c64
Fox Lin <likeszm@foxmail.com>
2026-06-07 18:15:05 +0800

    feat(T274): 新增H指数功能实现

---
+++ hot150/T274.cpp
@@ -0,0 +1,16 @@
+// T274.H指数
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+ public:
+  int hIndex(vector<int>& citations) {
+    sort(citations.begin(), citations.end(), greater<int>());
+    for (int i = 0; i < citations.size(); ++i) {
+      if (i + 1 > citations[i]) return i;
+    }
+    return citations.size();
+  }
+};
