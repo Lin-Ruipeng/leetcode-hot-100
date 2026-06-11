71d2252af6371c055c97bee5e3b0c614c09972e3
Fox Lin <likeszm@foxmail.com>
2026-06-11 14:24:13 +0800

    feat(T236): 新增二叉树的最近公共祖先实现

---
+++ Carl/Chatper08part2/T236.cpp
@@ -0,0 +1,25 @@
+// T236.二叉树的最近公共祖先
+
+struct TreeNode {
+  int val;
+  TreeNode* left;
+  TreeNode* right;
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+};
+
+class Solution {
+ public:
+  TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+    if (root == q || root == p || root == nullptr) return root;
+    TreeNode* left = lowestCommonAncestor(root->left, p, q);
+    TreeNode* right = lowestCommonAncestor(root->right, p, q);
+    if (left && right) return root;
+
+    if (!left && right)
+      return right;
+    else if (left && !right)
+      return left;
+    else
+      return nullptr;
+  }
+};
