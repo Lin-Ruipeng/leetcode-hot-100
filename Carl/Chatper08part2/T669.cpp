fe50109bf2af19dff097fa7bdf49c7dba9df8b0b
Fox Lin <likeszm@foxmail.com>
2026-06-18 16:18:27 +0800

    feat(T669): 新增层序遍历法修剪二叉搜索树的实现

--- Carl/Chatper08part2/T669.cpp
+++ Carl/Chatper08part2/T669.cpp
@@ -31,3 +31,40 @@ class Solution {
     return root;
   }
 };
+
+// 层序遍历
+class Solution2 {
+ public:
+  TreeNode* trimBST(TreeNode* root, int low, int high) {
+    if (!root) {
+      return nullptr;
+    }
+
+    // 处理头节点,左右都闭区间
+    while (root && (root->val < low || root->val > high)) {
+      if (root->val < low) {
+        root = root->right;  // 小于low遍历右侧
+      } else {
+        root = root->left;  // 大于high就往左遍历
+      }
+    }
+    TreeNode* cur = root;
+    // 此时root已经在[low,high]闭区间内了,处理左子小于low
+    while (cur) {
+      while (cur->left && cur->left->val < low) {
+        cur->left = cur->left->right;
+      }
+      cur = cur->left;
+    }
+    cur = root;
+
+    // 再处理右子大于high
+    while (cur) {
+      while (cur->right && cur->right->val > high) {
+        cur->right = cur->right->left;
+      }
+      cur = cur->right;
+    }
+    return root;
+  }
+};
