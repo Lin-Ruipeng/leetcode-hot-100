6c748c666cc48d5f02fa1961c4a463f2dfccc19e
Fox Lin <likeszm@foxmail.com>
2026-06-20 12:05:44 +0800

    feat(T108): 新增层序遍历法将有序数组转换为二叉搜索树的实现

--- Carl/Chatper08part2/T108.cpp
+++ Carl/Chatper08part2/T108.cpp
@@ -32,3 +32,49 @@ class Solution {
     return root;
   }
 };
+
+// 层序遍历
+#include <queue>
+class Solution2 {
+ public:
+  TreeNode* sortedArrayToBST(vector<int>& nums) {
+    if (nums.size() == 0) {
+      return nullptr;
+    }
+
+    TreeNode* root = new TreeNode(0);  // 根节点
+    queue<TreeNode*> nodeQue;          // 遍历的节点
+    queue<int> leftQue;                // 左区间下标
+    queue<int> rightQue;               // 右区间下标
+    nodeQue.push(root);                // 先从根节点开始
+    leftQue.push(0);                   // 左边从0开始
+    rightQue.push(nums.size() - 1);    // 右边从最大开始
+
+    while (!nodeQue.empty()) {
+      TreeNode* curNode = nodeQue.front();
+      nodeQue.pop();
+      int left = leftQue.front();
+      leftQue.pop();
+      int right = rightQue.front();
+      rightQue.pop();
+      int mid = left + ((right - left) / 2);
+
+      curNode->val = nums[mid];
+
+      if (left <= mid - 1) {
+        curNode->left = new TreeNode(0);
+        nodeQue.push(curNode->left);
+        leftQue.push(left);
+        rightQue.push(mid - 1);
+      }
+
+      if (right >= mid + 1) {
+        curNode->right = new TreeNode(0);
+        nodeQue.push(curNode->right);
+        leftQue.push(mid + 1);
+        rightQue.push(right);
+      }
+    }
+    return root;
+  }
+};
