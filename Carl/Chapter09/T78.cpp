536ca5e39a1f9aab366dec0df983de4056e0a58e
Fox Lin <likeszm@foxmail.com>
2026-06-28 13:24:07 +0800

    feat(T78): 新增子集问题的回溯算法实现

---
+++ Carl/Chapter09/T78.cpp
@@ -0,0 +1,28 @@
+// T78.子集
+
+#include <vector>
+using namespace std;
+
+class Solution {
+  vector<vector<int>> result;
+  vector<int> path;
+  void backtracking(vector<int>& nums, int startIndex) {
+    result.push_back(path);
+    if (startIndex >= nums.size()) {
+      return;
+    }
+    for (int i = startIndex; i < nums.size(); ++i) {
+      path.push_back(nums[i]);
+      backtracking(nums, i + 1);
+      path.pop_back();
+    }
+  }
+
+ public:
+  vector<vector<int>> subsets(vector<int>& nums) {
+    result.clear();
+    path.clear();
+    backtracking(nums, 0);
+    return result;
+  }
+};
