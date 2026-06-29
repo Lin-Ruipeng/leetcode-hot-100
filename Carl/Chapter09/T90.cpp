a4f6654d4f62a139d447b5a2e9abfcbca51a1540
Fox Lin <likeszm@foxmail.com>
2026-06-29 20:12:45 +0800

    feat(T90): 新增子集II问题的回溯算法实现

---
+++ Carl/Chapter09/T90.cpp
@@ -0,0 +1,33 @@
+// T90.子集II
+
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+class Solution {
+  vector<vector<int>> result;
+  vector<int> path;
+  void backtracking(vector<int>& nums, int startIndex, vector<bool>& used) {
+    result.push_back(path);
+    for (int i = startIndex; i < nums.size(); ++i) {
+      if (i > 0 && nums[i] == nums[i - 1] && used[i - 1] == false) {
+        continue;
+      }
+      path.push_back(nums[i]);
+      used[i] = true;
+      backtracking(nums, i + 1, used);
+      used[i] = false;
+      path.pop_back();
+    }
+  }
+
+ public:
+  vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+    result.clear();
+    path.clear();
+    vector<bool> used(nums.size(), false);
+    sort(nums.begin(), nums.end());
+    backtracking(nums, 0, used);
+    return result;
+  }
+};
