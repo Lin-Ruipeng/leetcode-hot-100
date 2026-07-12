a3f5747d04cc17ad290d5b27cb530845cc217f23
Fox Lin <likeszm@foxmail.com>
2026-07-12 11:13:22 +0800

    feat(T738): 新增单调递增的数字问题的解决方案

---
+++ Carl/Chapter10/T738.cpp
@@ -0,0 +1,21 @@
+// 738. 单调递增的数字
+#include <string>
+using namespace std;
+
+class Solution {
+ public:
+  int monotoneIncreasingDigits(int n) {
+    string strNum = to_string(n);
+    int flag = strNum.size();
+    for (int i = strNum.size() - 1; i > 0; --i) {
+      if (strNum[i - 1] > strNum[i]) {
+        flag = i;
+        strNum[i - 1]--;
+      }
+    }
+    for (int i = flag; i < strNum.size(); ++i) {
+      strNum[i] = '9';
+    }
+    return stoi(strNum);
+  }
+};
