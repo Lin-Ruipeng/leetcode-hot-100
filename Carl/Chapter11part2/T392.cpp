02f837135772b76bd446256c2934ae91ce08c485
Fox Lin <likeszm@foxmail.com>
2026-08-04 19:09:38 +0800

    feat(T392): 新增判断子序列问题的解决方案

---
+++ Carl/Chapter11part2/T392.cpp
@@ -0,0 +1,23 @@
+// T392. 判断子序列
+
+#include <string>
+#include <vector>
+using namespace std;
+
+class Solution {
+ public:
+  bool isSubsequence(string s, string t) {
+    vector<vector<int>> dp(s.size() + 1, vector<int>(t.size() + 1, 0));
+    for (int i = 1; i <= s.size(); ++i) {
+      for (int j = 1; j <= t.size(); ++j) {
+        if (s[i - 1] == t[j - 1]) {
+          dp[i][j] = dp[i - 1][j - 1] + 1;
+        } else {
+          dp[i][j] = dp[i][j - 1];
+        }
+      }
+    }
+    if (dp[s.size()][t.size()] == s.size()) return true;
+    return false;
+  }
+};
